87bb2bf9ada8338b4aee9175decfbe6a11532070
amiraamgad5@gmail.com <amiramostafa@Amiras-MBP.lan>
2018-10-20 18:45:54 +0200

    pushing a new repo

---
+++ javascript/javascript2/week1/main.js
@@ -0,0 +1,13 @@
+const obj = JSON.parse(
+  `[{
+    "name": "rohit",
+    "age": "null"
+   }, {
+    "name": "mikael",
+    "pet name": "mike",
+    "age": 
+   }]`
+);
+console.log(obj);
+
+console.log(repos);
